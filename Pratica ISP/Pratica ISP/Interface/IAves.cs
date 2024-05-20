076362780cf4decf0e0ab6433f9a12e16b032b47
starling188 <gonzalezmiranda188@gmail.com>
2024-05-20 14:37:48 -0400

    Agregar archivos de proyecto.

---
+++ Pratica ISP/Pratica ISP/Interface/IAves.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_ISP.Interface
+{
+    public interface IAves
+    {
+        void Cantar();
+        void PonerHuevos();
+    }
+}
---
+++ Pratica ISP/Pratica ISP/Interface/IVolar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_ISP.Interface
+{
+    public interface IVolar
+    {
+        void Volar();
+    }
+}
